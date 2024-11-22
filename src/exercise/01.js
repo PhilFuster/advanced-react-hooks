c9eb91f3cc15233f474fce4dda01cdf041403f98
Philip Fuster <pafus23@protonmail.com>
2024-11-22 08:10:09 -0500

    Exercise 1. Extra Credit 4

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,24 +4,29 @@
 import * as React from 'react'
 
 function countReducer(state, action) {
-  if (typeof action === 'function') {
-    return {...state, ...action(state)}
-  } else if (typeof action === 'object') {
-    return {...state, ...action}
+  const {type, step} = action
+  const {count} = state
+
+  switch (type) {
+    case 'INCREMENT': {
+      return {
+        count: count + step,
+      }
+    }
+    default: {
+      throw new Error(`Invalid action type for countReducer: ${type}`)
+    }
   }
-  throw new Error(
-    `Unsupported type for parameter 'action': '${typeof action}'. Supported types are 'object' and 'function'`,
-  )
 }
 
 function Counter({initialCount = 0, step = 1}) {
-  const [state, setState] = React.useReducer(countReducer, {
+  const [state, dispatch] = React.useReducer(countReducer, {
     count: initialCount,
   })
 
   const {count} = state
 
-  const increment = () => setState('test')
+  const increment = () => dispatch({type: 'INCREMENT', step})
   return <button onClick={increment}>{count}</button>
 }
 
