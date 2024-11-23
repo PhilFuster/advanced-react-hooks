874229f01e1b452bba8b5fd0d8527628f3415fa6
Philip Fuster <pafus23@protonmail.com>
2024-11-22 20:52:34 -0500

    Exercise 3 Extra Credit 1. Create a consumer hook

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -3,7 +3,7 @@
 
 import * as React from 'react'
 
-const CountContext = React.createContext()
+const CountContext = React.createContext([0, () => {}])
 
 function CountProvider({children, ...restProps}) {
   const [count, setCount] = React.useState(0)
@@ -16,13 +16,23 @@ function CountProvider({children, ...restProps}) {
   )
 }
 
+function useCount() {
+  const context = React.useContext(CountContext)
+
+  if (!context) {
+    throw new Error('useCount must be used within the CountProvider')
+  }
+
+  return context
+}
+
 function CountDisplay() {
-  const [count] = React.useContext(CountContext)
+  const [count] = useCount()
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
-  const [, setCount] = React.useContext(CountContext)
+  const [, setCount] = useCount()
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
